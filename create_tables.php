cc2183195af7dbd0605720bcc1b02479f5448cb7
isunitha98selvan <sunitha98selvan@gmail.com>
2018-10-29 15:25:06 +0530

    Create Table

---
+++ create_tables.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "10.52.56.55";
+$username = "user";
+$password = "Student@123";
+$dbname = "movies";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
