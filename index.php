7b6030b1489d0d17b09bccd67aa1357eb5d9cfa3
isunitha98selvan <sunitha98selvan@gmail.com>
2018-10-29 15:28:52 +0530

    folders added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php"; ?>
 	
-		<a href="create.php"><strong></strong> Register as new User</a><br>
-		<a href="read.php"><strong>Login as existing User</strong></a> 
+		<a href="pages/register_user.php"><strong></strong> Register as new User</a><br>
+		<a href="pages/login.php"><strong>Login as existing User</strong></a> 
 <?php include "templates/footer.php"; ?>
 
 
